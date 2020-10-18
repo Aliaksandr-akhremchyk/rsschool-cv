74f57d7b92cab4c06d6ea6d7f34fa657109508c9
Aliaksandr Akhremchyk <Alex_1984_05@mail.ru>
2020-10-18 13:50:39 +0300

    refactor: update structure

--- Simple-singolo/script.js
+++ Simple-singolo/script.js
@@ -13,9 +13,11 @@ let arr2 = [];
 for(var i = arr1.length - 1; i >= 0; i--){
         arr2[i] = a;
 }
-TAB.addEventListener('click', (event) =>{
+TAB.querySelectorAll('li').forEach(el => {
+el.addEventListener('click', (event) =>{
     TAB.querySelectorAll('li').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
+    MIXED.querySelectorAll('img').forEach(el => el.classList.remove('border-img'));
     MIXED.querySelectorAll('img').forEach(el => el.remove());
     // function mix(arr1){
         for (var i = arr1.length - 1; i > 0; i--) {
@@ -37,6 +39,22 @@ TAB.addEventListener('click', (event) =>{
     }
     ++a;   
 });
+});
+
+// MIXED.addEventListener('click', (event) =>{
+//     MIXED.querySelectorAll('img').forEach(el => el.classList.remove('border-img'));
+//     event.target.classList.add('border-img');
+// });
+
+MIXED.querySelectorAll('img').forEach(el => {
+el.addEventListener('click', (event) => {
+    MIXED.querySelectorAll('img').forEach(el => el.classList.remove('border-img'));
+        event.target.classList.add('border-img');
+    });
+});
+    
+    
+
 
 const MENUB = document.getElementById('burger');
 MENUB.addEventListener('click', (event) =>{
@@ -52,9 +70,56 @@ MENUB.addEventListener('click', (event) =>{
 
 const MENUC = document.getElementById('buttun');
 MENUC.addEventListener('click', () =>{
+    if(MENUC.getAttribute('class') == 'rotate') {
+        document.getElementById('overlay-tint').classList.add('hiden');
+        document.getElementById('burger-menu').classList.remove('move');
+        document.getElementById('menu-icon').classList.remove('doun');
+        document.getElementById('buttun').classList.remove('rotate');
+        document.getElementById('h1').classList.remove('hiden');
+    } else {
     document.getElementById('h1').classList.add('hiden');
     document.getElementById('overlay-tint').classList.remove('hiden');
     document.getElementById('burger-menu').classList.add('move');
     document.getElementById('menu-icon').classList.add('doun');
     MENUC.classList.add('rotate');
+    }
 });
+
+window.addEventListener(`resize`, event => {
+    if(window.innerWidth > 768 && (MENUC.getAttribute('class') == 'rotate')) {
+        document.getElementById('overlay-tint').classList.add('hiden');
+        document.getElementById('burger-menu').classList.remove('move');
+        document.getElementById('menu-icon').classList.remove('doun');
+        document.getElementById('buttun').classList.remove('rotate');
+        document.getElementById('h1').classList.remove('hiden');
+    }
+
+});
+
+
+document.addEventListener('scroll', onScrol);
+function onScrol() {
+    const POS = window.scrollY;
+    const DIVS = document.querySelectorAll('body>div');
+
+    DIVS.forEach((el) => {
+        if(el.offsetTop - 95 <= POS && (el.offsetTop - 95 + el.offsetHeight) > POS){
+            MENU.querySelectorAll('a').forEach((a) => {
+                a.classList.remove('active');
+                if (el.getAttribute('class').slice(0, 4) === a.getAttribute('class').slice(0, 4)) {
+                    a.classList.add('active');
+                }
+            })
+        }
+    });
+    DIVS.forEach((el) => {
+        if(el.offsetTop - 95 <= POS && (el.offsetTop - 95 + el.offsetHeight) > POS){
+            MENUB.querySelectorAll('a').forEach((a) => {
+                a.classList.remove('active');
+                if (el.getAttribute('class').slice(0, 4) === a.getAttribute('class').slice(0, 4)) {
+                    a.classList.add('active');
+                }
+            })
+        }
+    });
+}
